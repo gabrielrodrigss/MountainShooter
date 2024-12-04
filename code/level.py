39c24aaf0e0b19b17d49a7c83f7237a5a30cd2d3
Gabriel Oliveira Rodrigues <gabrielrrodrigues133@gmail.com>
2024-12-04 18:15:41 -0300

    Fixing imports in level.py file

--- code/level.py
+++ code/level.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import pygame.display
+import sys
+from msilib.schema import Font
 
+import pygame.display
+from pygame import Surface, Rect
+
+from code.Const import COLOR_WHITE, WIN_HEIGTH
 from code.entity import Entity
 from code.entityFactory import EntityFactory
 
@@ -13,11 +18,32 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.timeout = 20000 # 20 segundos
 
     def run(self, ):
+        pygame.mixer_music.load(f'./asset/{self.name}.mp3')
+        pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(60) # fps, altera o a velocidade de atualização do background
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # printed text
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', COLOR_WHITE, (10, 5)) # tempo de duração da fase
+            self.level_text(14, f'fps: {clock.get_fps() :.0f}s', COLOR_WHITE, (10, WIN_HEIGTH - 35)) # Impressão do fps
+            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGTH - 20)) # mostrar quantada entidade tem na tela. Uma depuração
             pygame.display.flip()
         pass
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont("Lucida Sans Typerwriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
+
