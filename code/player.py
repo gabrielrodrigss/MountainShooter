2a832d9a734680b883f7ab37b6fb79b73296cf66
Gabriel Oliveira Rodrigues <gabrielrrodrigues133@gmail.com>
2024-12-05 16:59:59 -0300

    Creating Players and Enemies Moving players and Enemies

--- code/player.py
+++ code/player.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.key
 
-from Entity import Entity
+from code.Const import ENTITY_SPEED, WIN_HEIGTH, WIN_WIDTH, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_RIGHT, \
+    PLAYER_KEY_LEFT
+from code.entity import Entity
 
 
 class Player(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0:
+            self.rect.centery -= ENTITY_SPEED[self.name]
+        if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGTH:
+            self.rect.centery += ENTITY_SPEED[self.name]
+        if pressed_key[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+        if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.left < WIN_WIDTH:
+            self.rect.centerx += ENTITY_SPEED[self.name]
         pass
